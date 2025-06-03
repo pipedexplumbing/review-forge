1a64d505bd583495e745cc426fb6e2bdafcc9712
Brandon Fertig <brandonfertig@gmail.com>
2025-06-03 20:32:19 +0000

    make it dark themed. make it look like the firebase brand actually, since it has orange like amazon and looks hip

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${belleza.variable} ${alegreya.variable}`}>
+    <html lang="en" className={`dark ${belleza.variable} ${alegreya.variable}`}>
       <head>
         {/* Google Fonts are now handled by next/font, no direct link needed here unless for other fonts */}
       </head>
